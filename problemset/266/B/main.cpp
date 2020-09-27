e3c8343674be47d37a129a0d937f91155eb91291
Le Nguyen Hoang Nhan <public.hoangnhanlenguyen@gmail.com>
2020-09-27 20:29:36 +0700

    Add solution for problem 266A

--- problemset/266/B/main.cpp
+++ problemset/266/B/main.cpp
@@ -1,15 +1,47 @@
-#include <iostream>
+#ifndef ONLINE_JUDGE
+#include "main.h"
+#else
+#include <bits/stdc++.h>
+int main();
+void solve();
+#endif
 
 int main() {
-    int childrenNum = 0;
-    std::cin >> childrenNum;
 
-    int second = 0;
-    std::cin >> second;
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
 
-    std::string students;
+#ifndef ONLINE_JUDGE
+    freopen("input", "r", stdin);
+    freopen("output", "w", stdout);
+    freopen("error", "w", stderr);
+#endif
 
+    solve();
+
+    std::cerr << "time taken : " << (float)clock() / CLOCKS_PER_SEC << " secs" << std::endl;
+    return 0;
+}
+
+void solve() {
+    int studentNum = 0;
+    std::cin >> studentNum;
+
+    int sec = 0;
+    std::cin >> sec;
+
+    std::string students = std::string();
     std::cin >> students;
 
-    for (; i < 0;
+    for (; sec > 0; --sec) {
+        for (int i = 0; i < studentNum - 1; ++i) {
+            if (students[i] == 'B' && students[i + 1] == 'G') {
+                students[i] = 'G';
+                students[i + 1] = 'B';
+                i = i + 1;
+            }
+        }
+    }
+
+    std::cout << students << std::endl;
 }
